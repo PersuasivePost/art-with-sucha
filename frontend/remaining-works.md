d67c87fd9eca0ac35bde552bfed31cb2dce59b63
PersuasivePost <codingash27@gmail.com>
2025-09-11 20:39:57 +0530

    1

---
+++ frontend/remaining-works.md
@@ -0,0 +1,1 @@
+1. add facebook link in footer
---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import Footer from '../../components/Footer/Footer';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Home page </h1>
+
+
+
+
+
+            <Footer />
+        </div>
+    )
+}
