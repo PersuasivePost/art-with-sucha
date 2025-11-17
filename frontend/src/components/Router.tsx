131d84383b2f03fce07e12fd0e52e6700f024457
Ashvatth Joshi <codingash27@gmail.com>
2025-11-17 12:36:09 +0530

    login frontend fixes

--- frontend/src/components/Router.tsx
+++ frontend/src/components/Router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home/Home";
-import AdminLogin from "../pages/Login/Adminlogin.tsx";
-import Login from "../pages/Login/Login.tsx";
+import AdminLogin from "../pages/Login/Adminlogin/Adminlogin.tsx";
+import Login from "../pages/Login/Userlogin/Login.tsx";
 import About from "../pages/AboutUs/About";
 import Contact from "../pages/ContactUs/Contact";
 import SectionView from "../pages/Gallery/Section/SectionView";
